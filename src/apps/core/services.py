820ab737928d7b5c4c49d485d6d8d3977f36b018
novel <18.novel.18@gmail.com>
2023-08-01 13:44:06 +0300

    fix
    Took 34 seconds

--- src/apps/core/services.py
+++ src/apps/core/services.py
@@ -1,25 +1,24 @@
-from src.apps.core.decorators import attribute_required
 
 
 class BaseService:
-    _repository = None
+    repository = None
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         if getattr(self, '_repository') is None:
             raise AttributeError(f"attribute _repository should not be None.")
 
     def create(self, **kwargs):
-        return self._repository.create(**kwargs)
+        return self.repository.create(**kwargs)
 
     def update_present(self, pk, **kwargs):
-        return self._repository.update_present(pk, **kwargs)
+        return self.repository.update_present(pk, **kwargs)
 
     def get_by_id(self, pk):
-        return self._repository.get_present_by_id(pk)
+        return self.repository.get_present_by_id(pk)
 
     def delete(self, pk):
-        obj = self._repository.get_by_id(pk)
-        self._repository.delete_present(obj)
+        obj = self.repository.get_by_id(pk)
+        self.repository.delete_present(obj)
 
 
 s = BaseService()
